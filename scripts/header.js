c31c28d4234cd5411e035ae1fb0bf4f109522a1e
s0rus <molik223@gmail.com>
2020-09-06 21:18:33 +0200

    expanded index page 🌷

--- scripts/header.js
+++ scripts/header.js
@@ -1,9 +1,22 @@
 const hamburger = document.querySelector('.hamburger');
 const options = document.querySelector('.option-wrapper');
 
+const quoteEl = document.querySelector('.quote');
+
+const quotes = ['"Zdanie SWI nigdy nie było łatwiejsze!" ~ Maciej K.', '"Sprawdzian stał się banalnie prosty." ~ Tomasz K.'];
+let quoteCounter = 0;
+
+const setQuote = () => {
+    quoteEl.textContent = quotes[quoteCounter];
+    quoteCounter >= 1 ? quoteCounter = 0 : quoteCounter++;
+};
+
+setInterval(setQuote, 6000);
+
 const handleClick = () => {
     hamburger.classList.toggle('hamburger--active');
     options.classList.toggle('menu-active');
 }
+
 
 hamburger.addEventListener('click', handleClick);
